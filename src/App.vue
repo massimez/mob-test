80e36c3ed94fcce372d50b2a46703ac550f6d362
Massi <massi.mez@ya.ru>
2022-07-27 17:24:08 +0700

    chore:init vuejs

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import CardProduct from "./components/CardProduct.vue";
+import ProductsList from "./components/ProductsList.vue";
+import SearchInput from "./components/UI/SearchInput.vue";
+</script>
+
+<template>
+  <header></header>
+
+  <main class="main">
+    <SearchInput></SearchInput>
+    <ProductsList></ProductsList>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.main {
+  padding: 0 5px;
+  background-color: white;
+  color: black;
+}
+</style>
